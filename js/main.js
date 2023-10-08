5d782960bb03e9efbca13ac72c6e739edfd34dd1
Gruvix <timmy.burton@hotmail.com>
2023-10-08 10:57:11 -0300

    preparado js funciones basicas
    armado de funciones basicas, sin desarrollar

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+
+let ronda = 0;
+let secuenciaComputadora = [];
+
+function comenzarJuego(){
+    reiniciar();
+    turnoCompu();
+}
+
+function reiniciar(){
+    //reiniciar
+}
+
+function turnoCompu(){
+    ronda ++;
+    secuenciaComputadora.push(randomColor());
+}
+
+function turnoJugador(){
+    //turno del jugador
+}
+
+function randomColor(){
+    //generar un color aleatorio
+}
