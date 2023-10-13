65377dfcc3a4681fdd6a58135bd1a6b16ad33a06
Gruvix <timmy.burton@hotmail.com>
2023-10-13 17:17:44 -0300

    Moved computer turn to new file
    now all computer turn's related script is in its own file

---
+++ js/computerTurn.js
@@ -0,0 +1,35 @@
+function startSequence(){
+    
+    computerSequence.push(randomColorBox());
+    computerSequence.forEach((colorBox, index) => {
+        const time = (index+0.5) * tic;
+        setTimeout(highLightColor, time, colorBox);
+        setTimeout(handleSound, time+(0.5*tic), colorBox);
+    });
+    const delay = (tic * computerSequence.length + 1*tic).toFixed(1);
+    setTimeout(roundHandler, delay,"player");
+    reduceCounterBy100Ms((delay))
+}
+
+function blockInput(){
+    document.querySelectorAll(".rectangle").forEach($rectangle => {
+            $rectangle.onclick = function(){
+                console.log("input bloqueado")
+            }
+    })
+}
+
+function randomColorBox(){
+    const colorsAmount = document.querySelectorAll(".rectangle").length;
+    const colorNumber = Math.floor(Math.random() * colorsAmount) + 1;
+    return `rectangle${colorNumber}`;
+}
+
+function reduceCounterBy100Ms(ultimoValor){
+    if(ultimoValor <= 0){
+        return;
+    }
+    const valorActual = ultimoValor - 100;
+    document.querySelector("#contador").textContent = (valorActual/1000).toFixed(1);
+    setTimeout(reduceCounterBy100Ms, 100, valorActual);
+}
